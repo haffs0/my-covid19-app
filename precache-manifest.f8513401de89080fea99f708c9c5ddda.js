da8fd29b0ed2af687f391a7d775207a44c72195f
haffs <abuafeezabolarinwa@yahoo.com>
2020-04-12 10:44:32 -0700

    Updates

---
+++ precache-manifest.f8513401de89080fea99f708c9c5ddda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edad7f92008e3ee518fd6617418f52f9",
+    "url": "/my-covid19-app/index.html"
+  },
+  {
+    "revision": "61c83989352fa820f271",
+    "url": "/my-covid19-app/static/css/2.e5b99fdb.chunk.css"
+  },
+  {
+    "revision": "604819cca5e55f16860c",
+    "url": "/my-covid19-app/static/css/main.b48510ec.chunk.css"
+  },
+  {
+    "revision": "61c83989352fa820f271",
+    "url": "/my-covid19-app/static/js/2.25c2c17a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-covid19-app/static/js/2.25c2c17a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604819cca5e55f16860c",
+    "url": "/my-covid19-app/static/js/main.5e7baea1.chunk.js"
+  },
+  {
+    "revision": "a1787c9bd048e9a82a1a",
+    "url": "/my-covid19-app/static/js/runtime-main.391b5e4e.js"
+  }
+]);
